d16f3d2f93e1f6c265afb104c30a7abb2cfd6fc7
lesssliemarie <lesssliemarie@gmail.com>
2015-05-04 09:31:47 -0500

    Finessing the css for the modal and creating request confirmation page

--- nebulab.io/js/nebulab.js
+++ nebulab.io/js/nebulab.js
@@ -22,11 +22,6 @@
 
 
 (function() {
-	// fadeOut timer for requestBetaSubmitted dialog
-	setTimeout(function() {
-		$('#requestBetaSubmitted').fadeOut(400);
-	}, 3000);
-
 	// modal trigger
 	$(".request-beta-btn").leanModal({ 
 		overlay : 0.4, 
@@ -35,67 +30,30 @@
  
 })();
 
-
-
-function validateEmail(e) {
-	var t=/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return t.test(e)
-}
-function validate() {
-	$("#error-message").text("");
-	var e = $("#email").val();
-	var f = $("#first_name").val();
-	var l = $("#last_name").val();
-	if (validateEmail(e)) {
-		userRegister(e,function(e) {
-			if (e) {
-				$("#requestBetaSubmitted").css("display","block");
-				setTimeout(function() {
-					$("#requestBetaSubmitted").fadeOut(400)
-				},3e3)
-				$("#contact-form").children("input").val("");
-				$("#requestBetaModal, #lean_overlay").hide();
-			} else {
-				$("#error-message").text("Sorry we could not saved your email address, try again later!");
-			}
-		});
-	} else {
-		$("#error-message").text("Type in a valid email address, ya goof!");
-	} return false;
-}
-function userRegister(e, f, l, t) {
-	var n=Appbase.ns("user").v(e);
-	$.get("http://ipinfo.io",function(r) {
-		var i = navigator.userAgent;
-		var s = r;
-		n.setData(
-		{
-			email: e,
-			first_name: f,
-			last_name: l,
-			followUp: false,
-			createDate: new Date,
-			BrowserData: i,
-			ip: s
-		},
-		function(e,n) {
-			if(!e) {
-				return t(true);
-			} else {
-				return t(false);
-			}
-		})
-	},"jsonp")
-}
-
-Appbase.credentials("private_beta","7c517d866eaa7cd2d0b71532d724653b");
-
-
-$(window).on("load resize",function(e){
+// resize teal block
+$(window).on('load',function(e){
     if (window.innerWidth > 1146) {
 		h2 = $('.value-prop-bullets').height() - 168;
 		$('.value-prop-headline').height(h2);
 	} else {
 		$('.value-prop-headline').height('inherit');
+	}
+});
+$(window).on('resize',function(e){
+    if (window.innerWidth > 1146) {
+		h2 = $('.value-prop-bullets').height() - 168;
+		$('.value-prop-headline').height(h2);
+	} else {
+		$('.value-prop-headline').height('inherit');
+	}
+});
+
+
+// move sign up modal if 2nd cta is clicked
+$('.btn-cta-2').click(function() {
+	if(window.innerWidth > 940) {
+		$('#requestBetaModal').css('top', '1600px');
+	} else {
+		$('#requestBetaModal').css('top', '2360px');
 	}
 });
